20f6e53a834e6109ed13d38c2aad3490de5cfe94
Kevin Okamoto <kevin.okamoto@sap.com>
2021-05-12 10:23:39 -0700

    Adding checkout page with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CheckoutPageComponent } from './pages/checkout-page/checkout-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: CheckoutPageComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
